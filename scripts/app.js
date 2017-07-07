53cd78f192da9deb3551f0c5b008b73e0c3a2424
kc657 <kvn.0218@gmail.com>
2017-07-07 14:44:50 -0700

    HW completed

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,25 @@
-$(document).on("ready", function(){
+console.log("Sanity Check");
 
-});
+$(document).on('ready', function () {
+  $('form').on('submit', function (doc) {
+    doc.preventDefault();
+    getGifs();
+  })
+})
+
+function getGifs () {
+  $.ajax({
+    method: "GET",
+    url: 'http://api.giphy.com/v1/gifs/search',
+    data: $("form").serialize(),
+    success: onSuccess,
+  });
+}
+
+
+function onSuccess(gifData) {
+  $(".gif-img").remove();
+  gifData.data.forEach(function(v,i){
+    $(".gif-gallery").append($("<img class='img-responsive img-thumbnail gif-img' src="+v.images.fixed_height.url+">"));
+  })
+}
